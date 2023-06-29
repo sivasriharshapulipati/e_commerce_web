6499ba68b9c41fd590b41bf01ee0fa5394e40eee
Siva Sri Harsha Pulipati <117675778+sivasriharshapulipati@users.noreply.github.com>
2023-06-29 13:42:29 +0530

    28:01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
-
+// import reat router dom 
+import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+//import pages 
+import Home from './pages/Home';
+import ProductionDetails from './pages/ProductDetails';
+//import components 
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import Footer from './components/Footer';
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return <div className='overflow-hidden'>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path = '/' element={<Home/>}/>
+        <Route path = '/product/:id' element =
+        {<ProductionDetails />}/>
+      </Routes>
+      <Sidebar/>
+      <Footer/>
+    </Router>
+  </div>;
 };
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
-import React from 'react';
-
+import React , {useContext} from 'react';
+//import product context
+import { ProductContext } from '../contexts/ProductContext';
+// import components 
+import Product from '../components/Product';
 const Home = () => {
-  return <div>Homepage</div>;
+  // get products from product context
+  const {products} = useContext(ProductContext);
+  //get only mens &womans clothing catigories 
+  const filterestProducts =products.filter(items =>{
+    return (items.category === "men's clothing" || items.category === "women's clothing"
+    );
+  })
+  return <div>
+    <section className='py-16'>
+      <div className='container mx-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-col-5 gap-[30px] max-w-sm max-auto md:max-w-none and md:max-0'>
+          {filterestProducts.map(products => {
+            // return <div className='w-full h-[300px] bg-pink-200' key ={products.id}>{products.title}</div>
+            return (
+              <Product products={products} key={products.id}/>
+                
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  </div>;
 };
 
 export default Home;
