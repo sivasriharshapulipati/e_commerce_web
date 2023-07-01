804467119602eab55df8984159ce2606e082e40d
Siva Sri Harsha Pulipati <117675778+sivasriharshapulipati@users.noreply.github.com>
2023-07-01 12:35:04 +0530

    fi

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 import WomanImg from '../img/woman_hero.png';
 import { Link } from 'react-router-dom'; 
 const Hero = () => {
-  return <section className='bg-pink-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24'>
+  return <section className=' h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24'>
     <div className='conatiner mx-auto flex justify-around h-full'>
       {/*text  */}
     <div className='flex flex-col'>
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -24,11 +24,18 @@ const {title,price,description,image} =product;
 return <section className='pt-32 pb-12 lg:pg-32 h-screem flex items-center'>
 <div className='container mx-auto'>
   {/*images & text wrapper */}
-  <div className='flex flex-col lg:flex-row items-centerz'>
+  <div className='flex flex-col lg:flex-row items-center'>
   {/*image */}
-  <div>image</div>
+  <div className='flex flex-col lg:flex-row items-center mb-8 lg:mb-0'>
+    <img className='max-w-[200px] lg:max-w-sm' src ={image} alt=''/>
+  </div>
   {/*text */}
-  <div>text</div>
+  <div className='flex-1 text-center lg:text-left'>
+    <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>{title} </h1>
+    <div className='text-xl text-red-500 font-medium mb-6'>${price}</div>
+  </div>
+  <p className='mb-8'>{description}</p>
+  <button onClick={()=> addToCart(product,product.id)} className='bg-primary py-4 px-8 text-white'>Add to Cart</button>
   </div>
 </div>
 </section>
